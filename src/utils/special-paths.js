9c82bf3e2408e8c16e8c5dc944516e48e07e2d2d
Dale Hamel <dale.hamel@shopify.com>
2025-01-24 13:54:30 -0500

    Use staging

--- src/utils/special-paths.js
+++ src/utils/special-paths.js
@@ -298,7 +298,7 @@ export function getDownloadRecipeForSourceFile(
       console.log("GOT sha: "+sha+ " svc: "+service_name);
       return {
         type: 'CORS_ENABLED_SINGLE_FILE',
-        url: `http://localhost:8000/src?repo=${service_name}&ref=${sha}&path=${path}`
+        url: `https://profile-staging.shopify.io/src?repo=${service_name}&ref=${sha}&path=${path}`
       };
 
     }
